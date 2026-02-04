c475bd64a9715d5d14487fbd92958eeaa2cb0194
Luca Gualandi <lgualandi@omniagroup.it>
2026-02-04 22:57:01 +0100

    comments

--- packages/callback_handler/lib/interfaces/i_callback_handler.dart
+++ packages/callback_handler/lib/interfaces/i_callback_handler.dart
@@ -1,17 +1,28 @@
 import '../types/callback_type.dart';
 
-/// Interface for handling callbacks
-/// [T] is the type of callback function to be handled
+/// Interface defining the contract for callback handlers.
+///
+/// Implement this interface to create custom callback handling behavior.
+///
+/// Type parameters:
+/// * [CallbackInputType] - The type of input accepted by callbacks
+/// * [CallbackReturnType] - The type of value returned by callbacks
 abstract interface class ICallbackHandler<CallbackInputType,
     CallbackReturnType> {
-  /// Register a callback
+  /// Registers a new callback function.
+  ///
+  /// The [callback] will be stored and invoked when [invoke] is called.
   void register(
       CallbackWithReturn<CallbackInputType, CallbackReturnType> callback);
 
-  /// Unregister a callback
+  /// Removes a previously registered callback.
+  ///
+  /// After unregistering, the [callback] will no longer be invoked.
   void unregister(
       CallbackWithReturn<CallbackInputType, CallbackReturnType> callback);
 
-  /// Invoke a registered callback
+  /// Invokes all registered callbacks with the given [input].
+  ///
+  /// All registered callbacks will receive the same [input] value.
   void invoke(CallbackInputType input);
 }
--- packages/callback_handler/lib/types/callback_type.dart
+++ packages/callback_handler/lib/types/callback_type.dart
@@ -1,1 +1,11 @@
+/// A function type for callbacks that accept an input and return a value.
+///
+/// Type parameters:
+/// * [I] - The input type
+/// * [R] - The return type
+///
+/// Example:
+/// ```dart
+/// CallbackWithReturn<String, int> callback = (input) => input.length;
+/// ```
 typedef CallbackWithReturn<I, R> = R Function(I);
